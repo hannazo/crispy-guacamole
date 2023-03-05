79011314fc59c7d66267292f62e9844adebe0228
hannazo <106128732+hannazo@users.noreply.github.com>
2023-03-04 21:58:32 -0500

    added manager subclass

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,5 +1,15 @@
+// function addManager(manager) {
+//     return`${manager.getName()}
+//     ${manager.getRole()}
+//     ${manager.getId()}
+//     ${manager.getEmail()}
+//     ${manager.getRole()}
+//     ${manager.getOfficeNumber()}`;
+// }
+
+
 function generateHTML(data) {
-    return`${data}`;
+   return `${JSON.stringify(data)}`
 };
 
 module.exports = generateHTML;
